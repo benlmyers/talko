716adb2727d34a285bd4600649f928d4d41c85b1
Ben Myers <benl.myers01@gmail.com>
2022-05-07 04:15:16 -0700

    🫠 App functionality

--- talko/ContentView.swift
+++ talko/ContentView.swift
@@ -6,11 +6,43 @@
 //
 
 import SwiftUI
+import EasyFirebase
 
 struct ContentView: View {
+    
+    @State var myMessage = ""
+    @State var allMessages = [String]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Your message:", text: $myMessage)
+            Button("Send message") {
+                let newMessage = "Hello, world!"
+                EasyFirestore.Retrieval.get(singleton: "TalkoData", ofType: TalkoData.self) { data in
+                    if var data = data {
+                        data.messages.append(myMessage)
+                        EasyFirestore.Storage.set(data)
+                    }
+                }
+            }
+            Button("Refresh messages") {
+                EasyFirestore.Retrieval.get(singleton: "TalkoData", ofType: TalkoData.self) { data in
+                    if let data = data {
+                        allMessages = data.messages
+                    }
+                }
+            }
+            VStack {
+                Text("All messages")
+                ForEach(allMessages.reversed(), id: \.self) { message in
+                    Text(message)
+                }
+            }
+        }
+        .padding()
+        .onAppear {
+            EasyFirebase.configure()
+        }
     }
 }
 
@@ -18,4 +50,13 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+class TalkoData: Singleton {
+    /// The id of the singleton; an auto-generated value.
+    var id = "TalkoData"
+    /// The date the singleton was created (will be the first time you launch the app!)
+    var dateCreated = Date()
+    /// The messages that have been sent by the user.
+    var messages = [String]()
 }
