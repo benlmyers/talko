b2e516dec0bf64a290adb1a8ed81c6df6277ad0d
Ben Myers <benl.myers01@gmail.com>
2022-05-06 17:29:02 -0700

    Initial Commit

---
+++ talko/talkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  talkoApp.swift
+//  talko
+//
+//  Created by Ben Myers on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct talkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
